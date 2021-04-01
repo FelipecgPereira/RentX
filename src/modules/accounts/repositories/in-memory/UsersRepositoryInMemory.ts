6281c1cca70a6a779940d5bad7e752cb60a99bdd
Felipe Pereira <felipepereiracg@gmail.com>
2021-04-01 20:39:28 -0300

    Criação de carros

--- src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -1,5 +1,6 @@
 import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUsersDTO";
-import { User } from "@modules/accounts/entities/Users";
+import { User } from "@modules/accounts/infra/typeorm/entities/Users";
+
 import { IUserRepository } from "../IUsersRepository";
 
 
