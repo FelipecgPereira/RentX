7e43a358af06ea00f0bf58d8776820d17fee8668
Felipe Pereira <felipepereiracg@gmail.com>
2021-04-13 11:38:25 -0300

    ajuste await

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -4,7 +4,7 @@ export default async (): Promise<Connection> =>{
 
     const defaultOptions = await getConnectionOptions();
 
-    return await createConnection(
+    return createConnection(
         Object.assign(defaultOptions,{
             database: process.env.NODE_ENV === "test" ? "rentx_test": defaultOptions.database
         })
