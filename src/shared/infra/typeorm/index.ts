0ef0b686a14074b21284e75dcd7b3dee52ce8355
Felipe Pereira <felipepereiracg@gmail.com>
2021-04-13 11:37:27 -0300

    ajuste orm

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,11 @@
 import {Connection, createConnection, getConnectionOptions} from "typeorm";
 
-export default async(host = "localhost"): Promise<Connection> =>{
+export default async (): Promise<Connection> =>{
 
     const defaultOptions = await getConnectionOptions();
 
-    return createConnection(
+    return await createConnection(
         Object.assign(defaultOptions,{
-            host: process.env.NODE_ENV === "test" ? "localhost": host,
             database: process.env.NODE_ENV === "test" ? "rentx_test": defaultOptions.database
         })
     )
